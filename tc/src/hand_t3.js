e8ef61c62e1df91bf76e5a29747406b9ff2d03eb
tanzeemc25 <tanzeem.c25@gmail.com>
2015-01-26 02:49:51 -0500

    added myo.js templates in temp dir tc

---
+++ tc/src/hand_t3.js
@@ -0,0 +1,45 @@
+		var log = document.getElementById('log')
+
+		var myo = Myo.create(0);
+
+		myo.on('pose', function(poseName){
+			console.log(poseName);
+		})
+
+		myo.on('arm_recognized', function(){
+			console.log('good!', this.id);
+		})
+
+		myo.on('arm_lost', function(){
+			console.log('bad', this.id);
+		})
+
+
+		myo.on('wave_left', function(){
+			console.log('wave Left!');
+		})
+		myo.on('fist', function(){
+			console.log('BT PLZ');
+			myo.requestBluetoothStrength();
+		})
+
+		myo.on('connected', function(){
+			setInterval(function(){
+				myo.requestBluetoothStrength();
+			}, 100);
+		})
+
+		myo.on('bluetooth_strength', function(BTS){
+			var width = ((BTS * -1 - 40 ) / 50 ) * 100  + '%';
+			$('#log').width(width);
+			//console.log(width);
+		})
+
+		myo.on('double_tap', function(){
+			this.zeroOrientation();
+			console.log('double tap') ;
+		});
+
+		myo.on('gyroscope', function(data){
+			if(data.y < -2) console.log('hit!');
+		});
