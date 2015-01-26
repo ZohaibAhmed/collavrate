a87471fe68132a194e0fd90bf810836a3028cdda
tanzeemc25 <tanzeem.c25@gmail.com>
2015-01-26 03:35:51 -0500

    added webgl template geometry cube, rotated using quaternion data from orientation event

--- tc/src/hand_t3.js
+++ tc/src/hand_t3.js
@@ -1,14 +1,97 @@
-// Create Myo object for device with id 0
-var myMyo = Myo.create(0);
+// Create WebGL scene
+var initScene = function () {
+    window.scene = new THREE.Scene();
+    window.renderer = new THREE.WebGLRenderer({
+        alpha: true
+    });
 
-myMyo.on('fingers_spread', function(edge){
-    if(!edge) return;
-    console.log("You spread your fingers!")
-});
+    window.renderer.setClearColor(0x000000, 1);
+    window.renderer.setSize(window.innerWidth, window.innerHeight);
 
-myMyo.on('orientation', function(data){
-	console.log(data.x);
-});
+    window.renderer.domElement.style.position = 'fixed';
+    window.renderer.domElement.style.top = 0;
+    window.renderer.domElement.style.left = 0;
+    window.renderer.domElement.style.width = '100%';
+    window.renderer.domElement.style.height = '100%';
+
+    document.body.appendChild(window.renderer.domElement);
+
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+    directionalLight.position.set( 0, 0.5, 1 );
+    window.scene.add(directionalLight);
+
+    window.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    window.camera.position.fromArray([0, 0, 250]);
+    window.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    window.addEventListener('resize', function () {
+
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.render(scene, camera);
+
+    }, false);
+
+    scene.add(camera);
+
+    var geometry = new THREE.BoxGeometry(150, 150, 50);
+    var material = new THREE.MeshPhongMaterial({color: 0x15bdde});
+    window.cube = new THREE.Mesh(geometry, material);
+    cube.position.set(0,0,0);
+    cube.castShadow = true;
+    cube.receiveShadow = true;
+    scene.add(cube);
+
+    renderer.render(scene, camera);
+};
+
+
+// Initiate the MYO
+var initMyo = function() {
+    "use strict";
+
+	// Create Myo object for device with id 0
+	var myMyo = Myo.create(0);
+
+    window.quaternion = new THREE.Quaternion();
+
+
+	// Events
+	myMyo.on('fingers_spread', function(edge){
+	    if(!edge) return;
+	    console.log("You spread your fingers!")
+	});
+
+	myMyo.on('orientation', function(data){
+		
+		//console.log(data);
+		
+		window.quaternion.x = data.y;
+        window.quaternion.y = data.z;
+        window.quaternion.z = -data.x;
+        window.quaternion.w = data.w;
+
+ 		if(!window.baseRotation) {
+            window.baseRotation = quaternion.clone();
+            window.baseRotation = window.baseRotation.conjugate();
+        }
+
+        window.quaternion.multiply(baseRotation);
+        window.quaternion.normalize();
+        window.quaternion.z = -quaternion.z;
+
+        window.cube.setRotationFromQuaternion(window.quaternion);
+
+        renderer.render(scene, camera);
+
+
+	});
+};
+
+initScene();
+initMyo();
+
 
 // var log = document.getElementById('log')
 
