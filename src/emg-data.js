cd571d0456f153e57f636daa2d274a0c036ddd94
Zohaib Ahmed <zohaib92@gmail.com>
2015-02-04 12:47:44 -0500

    updating the emg data graphs

--- src/emg-data.js
+++ src/emg-data.js
@@ -7,123 +7,67 @@ function toggle() {
 window.data_collected = [];
 
 window.hub = new Myo.Hub();
-window.hub.on('frame', function(frame) { 
-  if (window.start) {
-    // push a new data point onto the back
-    $("#container").append("<div>" + frame.emg[0] + ", " +
-                                   frame.emg[1] + ", " +
-                                   frame.emg[2] + ", " +
-                                   frame.emg[3] + ", " +
-                                   frame.emg[4] + ", " +
-                                   frame.emg[5] + ", " +
-                                   frame.emg[6] + ", " +
-                                   frame.emg[7]
-                                    + "</div>");
 
-    window.data_collected.push(frame.emg);
-  }
+var createGraph = function(elementId, startingData, range, resolution){
+  var history = _.times(resolution, function(){
+    return startingData;
+  });
+  var graph = {
+    history : history,
+    getGraphData : function(){
+      var result = {};
+      _.each(this.history, function(data, index){
+        _.each(data, function(val, axis){
+          result[axis] = result[axis] || {label : axis, data : []};
+          result[axis].data.push([index, val])
+        });
+      });
+      return _.values(result);
+    },
+    addData : function(data){
+      this.history.push(data);
+      this.history = this.history.slice(1);
+      this.update();
+    },
+    update : function(){
+      this.plot.setData(this.getGraphData());
+      this.plot.draw();
+    },
+  };
+  graph.plot = $.plot("#" + elementId, graph.getGraphData(), {
+    series: {shadowSize: 0},
+    colors: [ '#84FFF1'],
+    xaxis: {
+      show: false,
+      min : 0,
+      max : resolution
+    },
+    yaxis : {
+      min : -range,
+      max : range
+    },
+    legend : {
+      show : false,
+    },
+    grid : {
+      borderColor : "#427F78"
+    }
+  });
+  return graph;
+}
+
+var podGraphs = _.times(8, function(num){
+  return createGraph('pod' + num, {data:0}, 128, 50);
+})
+
+var throttledEMGUpdate = _.throttle(function(data){
+  _.each(data, function(emgData, index){
+    podGraphs[index].addData({data:emgData});
+  })
+}, 20);
+
+window.hub.on('frame', function(frame) { 
+  throttledEMGUpdate(frame.emg);
 });
 
-function plot() {
-  $("#container").hide();
 
-  var collect_data = function(index) {
-    return_array = [];
-    for (i = 0; i < window.data_collected.length; i++) {
-      return_array.push(window.data_collected[i][index]);
-    }
-
-    return return_array;
-  }
-
-  var lineChartData = {
-    labels: Array.apply(null, {length: 100}).map(Number.call, Number),
-    datasets : [
-      {
-        label: "1",
-        fillColor : "rgba(220,220,220,0.2)",
-        strokeColor : "rgba(160,40,228,1)",
-        pointColor : "rgba(220,220,220,1)",
-        pointStrokeColor : "#fff",
-        pointHighlightFill : "#fff",
-        pointHighlightStroke : "rgba(220,220,220,1)",
-        data : collect_data(0)
-      },
-      {
-        label: "2",
-        fillColor : "rgba(151,187,205,0.2)",
-        strokeColor : "rgba(112,3,205,1)",
-        pointColor : "rgba(151,187,205,1)",
-        pointStrokeColor : "#fff",
-        pointHighlightFill : "#fff",
-        pointHighlightStroke : "rgba(151,187,205,1)",
-        data : collect_data(1)
-      },
-      {
-        label: "3",
-        fillColor : "rgba(151,187,205,0.2)",
-        strokeColor : "rgba(0,0,0,1)",
-        pointColor : "rgba(151,187,205,1)",
-        pointStrokeColor : "#fff",
-        pointHighlightFill : "#fff",
-        pointHighlightStroke : "rgba(151,187,205,1)",
-        data : collect_data(2)
-      },
-      {
-        label: "4",
-        fillColor : "rgba(151,187,205,0.2)",
-        strokeColor : "rgba(12,33,99,1)",
-        pointColor : "rgba(151,187,205,1)",
-        pointStrokeColor : "#fff",
-        pointHighlightFill : "#fff",
-        pointHighlightStroke : "rgba(151,187,205,1)",
-        data : collect_data(3)
-      },
-      {
-        label: "5",
-        fillColor : "rgba(151,187,205,0.2)",
-        strokeColor : "rgba(255,0,0,1)",
-        pointColor : "rgba(151,187,205,1)",
-        pointStrokeColor : "#fff",
-        pointHighlightFill : "#fff",
-        pointHighlightStroke : "rgba(151,187,205,1)",
-        data : collect_data(4)
-      },
-      {
-        label: "6",
-        fillColor : "rgba(151,187,205,0.2)",
-        strokeColor : "rgba(0,255,0,1)",
-        pointColor : "rgba(151,187,205,1)",
-        pointStrokeColor : "#fff",
-        pointHighlightFill : "#fff",
-        pointHighlightStroke : "rgba(151,187,205,1)",
-        data : collect_data(5)
-      },
-      {
-        label: "7",
-        fillColor : "rgba(151,187,205,0.2)",
-        strokeColor : "rgba(0,0,255,1)",
-        pointColor : "rgba(151,187,205,1)",
-        pointStrokeColor : "#fff",
-        pointHighlightFill : "#fff",
-        pointHighlightStroke : "rgba(151,187,205,1)",
-        data : collect_data(6)
-      },
-      {
-        label: "8",
-        fillColor : "rgba(151,187,205,0.2)",
-        strokeColor : "rgba(22,22,177,1)",
-        pointColor : "rgba(151,187,205,1)",
-        pointStrokeColor : "#fff",
-        pointHighlightFill : "#fff",
-        pointHighlightStroke : "rgba(151,187,205,1)",
-        data : collect_data(7)
-      }
-    ]
-  }
-
-  var ctx = document.getElementById("canvas").getContext("2d");
-  window.myLine = new Chart(ctx).Line(lineChartData, {
-    responsive: true
-  });
-}
