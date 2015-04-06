14f89176dee38ec29d077bce707e9871f3871426
Zohaib Ahmed <zohaib92@gmail.com>
2015-04-05 23:22:17 -0400

    Giving this project a new landing page

--- server/app.js
+++ server/app.js
@@ -8,6 +8,8 @@ var conString = config.conString;
 var app = express();
 var users = {};
 
+app.use(express.static('public'));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -15,7 +17,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function (req, res) {
-  res.send('Welcome to Collavrate')
+  res.sendfile('./views/index.html');
 });
 
 app.get('/world', function(req, res) {
